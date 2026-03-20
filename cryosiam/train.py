416759d2fd4ed8fd262bb12e9f23f754388095ad
Joshua Talks <joshua.talks@embl.de>
2026-03-20 09:35:16 +0100

    complete training script for self-supervised training

--- cryosiam/train.py
+++ cryosiam/train.py
@@ -1,17 +1,23 @@
 import os
 import torch
-from typing import assert_never
+import typer
+from typing import Annotated, assert_never
 import yaml
 
 from lightning import Trainer
 from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
 from lightning.pytorch.loggers import TensorBoardLogger, WandbLogger
+from lightning.pytorch.strategies import DDPStrategy
 
 from cryosiam.data_structure.dense_simsiam_config import load_dense_simsiam_config
 from cryosiam.module import DenseSimSiamModule
 
 
-def main(config_file_path):
+def main(
+    config_file_path: Annotated[
+        str, typer.Option(help="Path to config file", exists=True)
+    ],
+):
     cfg = load_dense_simsiam_config(config_file_path)
 
     is_distributed = cfg.parameters.gpu_devices > 1 or cfg.parameters.nodes > 1
@@ -24,7 +30,7 @@ def main(config_file_path):
     net = DenseSimSiamModule(cfg)
 
     checkpoint_callback = ModelCheckpoint(
-        dirpath=os.path.join(cfg.log_dir, "model"),
+        dirpath=os.path.join(cfg.logging.log_dir, "model"),
         filename="model_best",
         every_n_epochs=cfg.hyper_parameters.val_interval,
         monitor="val_loss",
@@ -45,3 +51,40 @@ def main(config_file_path):
 
     else:
         assert_never(cfg.logging.logger)
+
+    if is_distributed:
+        if cfg.hyper_parameters.cache_rate > 0:
+            use_distributed_sampler = False
+        else:
+            use_distributed_sampler = True
+        trainer = Trainer(
+            accelerator="gpu",
+            devices=cfg.parameters.gpu_devices,
+            num_nodes=cfg.parameters.nodes,
+            strategy=DDPStrategy(find_unused_parameters=True),
+            max_epochs=cfg.hyper_parameters.max_epochs,
+            use_distributed_sampler=use_distributed_sampler,
+            logger=logger,
+            check_val_every_n_epoch=cfg.hyper_parameters.val_interval,
+            callbacks=[checkpoint_callback, lr_monitor],
+        )
+    else:
+        trainer = Trainer(
+            accelerator="gpu",
+            devices=1,
+            max_epochs=cfg.hyper_parameters.max_epochs,
+            logger=logger,
+            check_val_every_n_epoch=cfg.hyper_parameters.val_interval,
+            callbacks=[checkpoint_callback, lr_monitor],
+        )
+
+    if cfg.continue_training:
+        trainer.fit(
+            net, ckpt_path=os.path.join(cfg.logging.log_dir, "model", "last.ckpt")
+        )
+    else:
+        trainer.fit(net)
+
+
+if __name__ == "__main__":
+    typer.run(main)
